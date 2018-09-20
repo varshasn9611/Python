72e9fe7ed4d6a7be3609ced453fcb79a958f42ef
varshasn9611 <43026230+varshasn9611@users.noreply.github.com>
2018-09-20 14:17:46 +0530

    Add files via upload

---
+++ mathematicaloperations.py
@@ -0,0 +1,13 @@
+#printing values
+a=50
+b=100
+c=25
+p=a+b+c
+print(p)
+q=a-b-c
+print(q)
+r=a*b*c
+print(r)
+s=a/b/c
+print(s)
+print("addition=",p,"\nsubstraction=",q,"\nmultiplication=",r,"\ndivision=",s)
