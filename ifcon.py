ae14844c4286791365720e877f20213ddfecc92c
varshasn9611 <43026230+varshasn9611@users.noreply.github.com>
2018-09-22 12:50:26 +0530

    Add files via upload

---
+++ ifcon.py
@@ -0,0 +1,10 @@
+#find greatest of two numbers
+a=int(input("enter the value of a: "))
+b=int(input("enter the value of b: "))
+#checking the condition 
+if(a>b):
+	print("a is greater than b")
+
+
+		
+
